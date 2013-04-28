7dd931421fc809c503fc409441b952aa54cf557f
Shulhi Sapli <shulhi@gmail.com>
2013-04-28 14:25:53 +0800

    changed Article model to support bool i/o 1 or 0

--- PocketNet/PocketNet/Models/Article.cs
+++ PocketNet/PocketNet/Models/Article.cs
@@ -11,28 +11,52 @@ namespace PocketNet.PocketNet.Models
     {
         [JsonProperty("item_id")]
         public int ItemId { get; set; }
+
         [JsonProperty("resolved_id")]
         public int ResolvedId { get; set; }
+
         [JsonProperty("given_url")]
         public string GivenUrl { get; set; }
+
         [JsonProperty("resolved_url")]
         public string ResolvedUrl { get; set; }
+
         [JsonProperty("given_title")]
         public string GivenTitle { get; set; }
+
         [JsonProperty("resolved_title")]
         public string ResolvedTitle { get; set; }
+
         [JsonProperty("favorite")]
         public int Favorite { get; set; }
+
         [JsonProperty("status")]
         public int Status { get; set; }
+
         [JsonProperty("excerpt")]
         public string Excerpt { get; set; }
-        [JsonProperty("is_article")]
-        public int IsArticle { get; set; }
-        [JsonProperty("has_image")]
-        public int HasImage { get; set; }
-        [JsonProperty("has_video")]
-        public int HasVideo { get; set; }
+
+        [JsonProperty("is_article")] private int _isArticle;
+        public bool IsArticle
+        {
+            get { return _isArticle == 1; }
+            set { _isArticle = value ? 1 : 0; }
+        }
+
+        [JsonProperty("has_image")] private int _hasImage;
+        public bool HasImage
+        {
+            get { return _hasImage == 1; }
+            set { _hasImage = value ? 1 : 0; }
+        }
+
+        [JsonProperty("has_video")] private int _hasVideo;
+        public bool HasVideo
+        {
+            get { return _hasVideo == 1; }
+            set { _hasVideo = value ? 1 : 0; }
+        }
+
         [JsonProperty("word_count")]
         public int WordCount { get; set; }
     }
