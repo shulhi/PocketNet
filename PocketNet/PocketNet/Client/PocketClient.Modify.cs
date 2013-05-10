35954a6b9794956bba4571ac324318c2c88aae5f
Shulhi Sapli <shulhi@gmail.com>
2013-05-10 20:13:15 +0800

    added readd, archive, delete item

--- PocketNet/PocketNet/Client/PocketClient.Modify.cs
+++ PocketNet/PocketNet/Client/PocketClient.Modify.cs
@@ -12,7 +12,7 @@ namespace PocketNet.PocketNet.Client
 {
     public partial class PocketClient
     {
-        public async Task MarkAsFavoriteAsync(params int[] itemId)
+        public async Task FavoriteItemAsync(params int[] itemId)
         {
             var items = itemId.Select(i => new {action = "favorite", item_id = i}).ToList();
 
@@ -24,7 +24,7 @@ namespace PocketNet.PocketNet.Client
                 });
         }
 
-        public async Task MarkAsUnfavoriteAsync(params int[] itemId)
+        public async Task UnfavoriteItemAsync(params int[] itemId)
         {
             var items = itemId.Select(i => new {action = "unfavorite", item_id = i}).ToList();
 
@@ -36,9 +36,33 @@ namespace PocketNet.PocketNet.Client
             });
         }
 
-        public async Task MarkAsDeleteAsync(params int[] itemId)
+        public async Task DeleteItemAsync(params int[] itemId)
         {
             var items = itemId.Select(i => new {action = "delete", item_id = i}).ToList();
+
+            var response = await GetValuesAsync<ItemModified>("v3/send", new
+            {
+                consumer_key = _consumerKey,
+                access_token = _accessToken,
+                actions = items
+            });
+        }
+
+        public async Task ArchiveItemAsync(params int[] itemId)
+        {
+            var items = itemId.Select(i => new { action = "archive", item_id = i }).ToList();
+
+            var response = await GetValuesAsync<ItemModified>("v3/send", new
+            {
+                consumer_key = _consumerKey,
+                access_token = _accessToken,
+                actions = items
+            });
+        }
+
+        public async Task ReaddItemAsync(params int[] itemId)
+        {
+            var items = itemId.Select(i => new { action = "readd", item_id = i }).ToList();
 
             var response = await GetValuesAsync<ItemModified>("v3/send", new
             {
